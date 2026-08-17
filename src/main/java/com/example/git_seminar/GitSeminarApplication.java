431e931c35ef69ca5bd44254bb334a048c5e4852
hscha-watchtek <hs1999@watchtek.co.kr>
2026-08-17 15:44:05 +0900

    [GitSeminar] Initial Commit

---
+++ src/main/java/com/example/git_seminar/GitSeminarApplication.java
@@ -0,0 +1,13 @@
+package com.example.git_seminar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSeminarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSeminarApplication.class, args);
+	}
+
+}
